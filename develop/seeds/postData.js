ee3fd6bae5f0508ed9c96b6b2cd846f0088b9a51
Mary Dillon <mary.dillon09@gmail.com>
2022-08-02 16:36:12 -0700

    creates model and seeds for post data

---
+++ develop/seeds/postData.js
@@ -0,0 +1,10 @@
+const { Post } = require('../models');
+
+const postData = [
+    {
+        title: 'Tech Blogs are Great',
+        content: 'Its great to read blogs',
+        publish_date: 'August 04, 2022 17:00:00'
+    }
+]
+
